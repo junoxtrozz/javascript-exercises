e538605e944c67964c9c2555bb3a00e9f96d6e31
Bryan Yale <junoxtrozz@gmail.com>
2020-02-06 17:56:50 -0500

    finished reverseString test

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,5 @@
 const reverseString = function(input) {
-    let stringArr = input.split("l");
+    let stringArr = input.split("");
     let reverseArr = stringArr.reverse();
     let reverseString = reverseArr.join('');
     return reverseString;
