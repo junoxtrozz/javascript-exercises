f3cea927ca0f97108b17ece51f123cdc680a0047
Bryan Yale <junoxtrozz@gmail.com>
2020-02-06 17:43:29 -0500

    completed helloWorld and repeatString, started reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,20 @@
-const repeatString = function() {
+const repeatString = function(input, num) {
+    if (num < 0)
+    {
+        return "ERROR"
+    }
+    else if (num === 0 )
+    {
+        return "";
+    }
+    else {
+        let finalString = input;
+        for(i = 1; i < num; i++) {
+            finalString += input;
+        }
+    
+        return finalString;
+    }
 
 }
 
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let stringArr = input.split("l");
+    let reverseArr = stringArr.reverse();
+    let reverseString = reverseArr.join('');
+    return reverseString;
 }
 
 module.exports = reverseString
