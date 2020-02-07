736e6c36ac3460438b97a34a7ee82be12da87e17
Bryan Yale <junoxtrozz@gmail.com>
2020-02-07 18:16:18 -0500

    Finished removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, toRemoveOne, toRemoveTwo, toRemoveThree, toRemoveFour) {
+    let myArr = arr;
+    let removeIndex;
+    let finalArr;
+    for(i = 1; i < arguments.length; i++)
+        {
+            removeIndex = myArr.indexOf(arguments[i]);
+            if(removeIndex != -1)
+                {
+                    finalArr = myArr.splice(removeIndex, 1);
+                }
+        }
+    
+    return myArr;
 
 }
 
