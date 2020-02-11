1151afc75fb2a3ea0bb8e1e84ce2e4f0d9bf8db5
Bryan Yale <junoxtrozz@gmail.com>
2020-02-11 18:30:59 -0500

    Finished tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let celsiusTemp = ((fTemp - 32) * (5/9));
+  if(celsiusTemp % 10 !== 0)
+  {
+    celsiusTemp = Math.round(celsiusTemp * 10) / 10;
+  }
+  return celsiusTemp;
 }
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let farenheitTemp = ((cTemp * (9/5)) + 32);
+  if(farenheitTemp % 10 !== 0)
+  {
+    farenheitTemp = Math.round(farenheitTemp * 10) / 10;
+  }
+  return farenheitTemp;
 }
 
 module.exports = {
