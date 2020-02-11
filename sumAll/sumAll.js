b58906c6a4c736f717dd1f0f7bccd5142248ef62
Bryan Yale <junoxtrozz@gmail.com>
2020-02-11 18:08:37 -0500

    finished sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    if((numOne > 0 && numTwo > 0) && (Number.isInteger(numOne) && Number.isInteger(numTwo)))
+    {
+        if(numOne < numTwo)
+        {
+            for(let i = numOne; i <= numTwo; i++)
+            {
+                sum += i;
+            }
+        }
+        else if(numOne > numTwo)
+        {
+            for(let i = numOne; i >= numTwo; i--)
+            {
+                sum += i;
+            }       
+        }
+    } else {
+        return "ERROR";
+    }
+    
+
+    return sum;
 
 }
 
